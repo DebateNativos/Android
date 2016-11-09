baacc23e18e6df58d9ae2ec41bd7742f8f57b55a
Jordan Wong <changwain22@gmail.com>
2016-11-09 01:35:44 -0600

    debater

--- ForumRIU/app/src/main/java/com/nativos/forumriu/DebaterActivity.java
+++ ForumRIU/app/src/main/java/com/nativos/forumriu/DebaterActivity.java
@@ -22,7 +22,12 @@ public class DebaterActivity extends AppCompatActivity implements View.OnClickLi
     NotificationCompat.Builder notification;
     Button btnNotification ;
     private int warning= 0;
-
+    ProgressBar mProgressBar, mProgressBar1;
+    private Button buttonStartTime, buttonStopTime;
+    private EditText edtTimerValue;
+    private TextView textViewShowTime;
+    private CountDownTimer countDownTimer;
+    private long totalTimeCountInMilliseconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +35,85 @@ public class DebaterActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_debater);
 
 
+        btnNotification=(Button) findViewById(R.id.buttonWarning);
         btnNotification.setText(getAllWarning(warning));
         notification = new NotificationCompat.Builder(this);
         notification.setAutoCancel(true);
+
+        buttonStartTime = (Button) findViewById(R.id.button_timerview_start);
+        buttonStopTime = (Button) findViewById(R.id.button_timerview_stop);
+
+        textViewShowTime = (TextView)
+                findViewById(R.id.textView_timerview_time);
+        edtTimerValue = (EditText) findViewById(R.id.textview_timerview_back);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar_timerview);
+        mProgressBar1 = (ProgressBar) findViewById(R.id.progressbar1_timerview);
+
+        buttonStartTime.setOnClickListener(this);
+        buttonStopTime.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.button_timerview_start) {
+
+            setTimer();
+
+            buttonStartTime.setVisibility(View.INVISIBLE);
+            buttonStopTime.setVisibility(View.VISIBLE);
+            mProgressBar.setVisibility(View.INVISIBLE);
+
+            startTimer();
+            mProgressBar1.setVisibility(View.VISIBLE);
+
+
+        } else if (v.getId() == R.id.button_timerview_stop) {
+            countDownTimer.cancel();
+            countDownTimer.onFinish();
+            mProgressBar1.setVisibility(View.GONE);
+            mProgressBar.setVisibility(View.VISIBLE);
+            edtTimerValue.setVisibility(View.VISIBLE);
+            buttonStartTime.setVisibility(View.VISIBLE);
+            buttonStopTime.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    private void setTimer(){
+        int time = 0;
+        if (!edtTimerValue.getText().toString().equals("")) {
+            time = Integer.parseInt(edtTimerValue.getText().toString());
+        } else
+            Toast.makeText(DebaterActivity.this, "Digite los segundos...",
+                    Toast.LENGTH_LONG).show();
+        totalTimeCountInMilliseconds =  time * 1000;
+        mProgressBar1.setMax( time * 1000);
+    }
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(totalTimeCountInMilliseconds, 1) {
+            @Override
+            public void onTick(long leftTimeInMilliseconds) {
+                long seconds = leftTimeInMilliseconds / 1000;
+                mProgressBar1.setProgress((int) (leftTimeInMilliseconds));
+
+                textViewShowTime.setText(String.format("%02d", seconds / 60)
+                        + ":" + String.format("%02d", seconds % 60));
+            }
+
+            @Override
+            public void onFinish() {
+                textViewShowTime.setText("00:00");
+                textViewShowTime.setVisibility(View.VISIBLE);
+                buttonStartTime.setVisibility(View.VISIBLE);
+                buttonStopTime.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.VISIBLE);
+                mProgressBar1.setVisibility(View.GONE);
+
+            }
+        }.start();
     }
 
     public String getAllWarning(int num){
