c0febc5fddbaa1d7c4132d3050fb01330fd6f081
Jordan Wong <changwain22@gmail.com>
2016-11-11 12:45:11 -0600

    icons

--- ForumRIU/app/src/main/java/com/nativos/forumriu/InsertCodeActivity.java
+++ ForumRIU/app/src/main/java/com/nativos/forumriu/InsertCodeActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.id.edit;
 
 public class InsertCodeActivity extends Activity {
 
@@ -18,23 +23,43 @@ public class InsertCodeActivity extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_insert_code);
 
-       getDebateName();
+        getDebateName();
+        validateCode();
+    }
+
+    public void validateCode() {
+
+        Button btnInsertCode = (Button) findViewById(R.id.StudentButton);
+
+        btnInsertCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editTextDebateCode);
+                String debateCode = editText.getText().toString().trim();
+                if (debateCode.equals("debatiente")) {
+                    goToDebaterActivity();
+                } else
+                    Toast.makeText(InsertCodeActivity.this, "Código Incorrecto ", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
 
-    public void getDebateName(){
+    public void getDebateName() {
 
-        Bundle debateData= getIntent().getExtras();
-        if(debateData==null){
+        Bundle debateData = getIntent().getExtras();
+        if (debateData == null) {
             return;
         }
 
-        String getName= debateData.getString("DebateName");
-        final TextView finalText=(TextView)findViewById(R.id.textViewDebateName);
+        String getName = debateData.getString("DebateName");
+        final TextView finalText = (TextView) findViewById(R.id.textViewDebateName);
         finalText.setText(getName);
     }
 
-    public void goToDebaterActivity(View view) {
+    public void goToDebaterActivity() {
         Intent intent = new Intent(getBaseContext(), DebaterActivity.class);
         startActivity(intent);
     }
