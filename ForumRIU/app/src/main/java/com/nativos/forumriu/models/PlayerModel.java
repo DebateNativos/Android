38bb0b6a4d3a255d08b035847fc5ba82eba34e08
Jordan Wong <changwain22@gmail.com>
2016-11-25 16:44:15 -0600

    role

---
+++ ForumRIU/app/src/main/java/com/nativos/forumriu/models/PlayerModel.java
@@ -0,0 +1,45 @@
+package com.nativos.forumriu.models;
+
+/**
+ * Created by Jordan on 25-Nov-16.
+ */
+
+public class PlayerModel {
+
+    int role;
+    int warnings;
+    int debate;
+
+    public PlayerModel() {
+    }
+
+    public PlayerModel(int debate, int role, int warnings) {
+        this.debate = debate;
+        this.role = role;
+        this.warnings = warnings;
+    }
+
+    public int getDebate() {
+        return debate;
+    }
+
+    public void setDebate(int debate) {
+        this.debate = debate;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getWarnings() {
+        return warnings;
+    }
+
+    public void setWarnings(int warnings) {
+        this.warnings = warnings;
+    }
+}
